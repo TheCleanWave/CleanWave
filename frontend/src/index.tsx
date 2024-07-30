f4e47c395125ea9e427c45c018409227c422cbb9
Preciado <aarongarpre@gmail.com>
2024-07-30 16:07:10 +0200

    React initialization

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,1 +1,8 @@
-alert("react here")
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+
+import App from './App';
+
+const element: Element = document.getElementById("app") as Element;
+const root = createRoot(element);
+root.render(<App/>);
